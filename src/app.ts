fd340b92b14d82303da3505124b0081decd8d509
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-08-25 22:12:41 +0400

    secp256k1

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,11 @@ import { scalarMultiplication } from './utils/scalar-multiplication';
 import { addressGenerate } from './utils/address.generate';
 // import { privateKeyToWIF } from './utils/private-key-to-wif';
 
+const q = {
+  x: 0x3E5AACFC20C77F3A6B24AED45D7CDA7AD889379681EB1C7968AABA1F5E28A78Fn,
+  y: 0x9B1ED65C930A32375FF8C16A2204A5F63FEA35404E5084A720AD1A1A4333E0E3n
+};
+
 const k = 0x000000000000000000000000000000000000000000000000000000000000000An;
 const Q = scalarMultiplication(k, params.G);
 
