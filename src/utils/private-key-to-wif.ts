c94b84127b7113e26103bceb0226edfdff0e2abe
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-09-10 13:02:44 +0400

    layout fix

--- src/utils/private-key-to-wif.ts
+++ src/utils/private-key-to-wif.ts
@@ -1,6 +1,9 @@
 import { base58Encode, sha256 } from './hash';
 
-export const privateKeyToWIF = (privateKey: bigint, compressed: boolean): string => {
+export const privateKeyToWIF = (
+  privateKey: bigint,
+  compressed: boolean
+): string => {
   const privateKeyHex = privateKey.toString(16).padStart(64, '0');
   const buffer = Buffer.from(privateKeyHex, 'hex');
   const prefix = Buffer.from([0x80]);
