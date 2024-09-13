b0f9ba762b3cc3c9999eb395dd831967947bfcba
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-09-13 11:04:51 +0400

    install elliptic lib

---
+++ src/utils/elliptic.class.ts
@@ -0,0 +1,21 @@
+import { ec as EC } from 'elliptic';
+
+const ec = new EC('secp256k1');
+
+export class Point {
+  x: bigint;
+  y: bigint;
+
+  constructor(x: bigint, y: bigint) {
+    this.x = x;
+    this.y = y;
+  }
+
+  toEllipticPoint() {
+    return ec.curve.point(this.x.toString(16), this.y.toString(16));
+  }
+
+  static fromEllipticPoint(point: any): Point {
+    return new Point(BigInt('0x' + point.getX().toString(16)), BigInt('0x' + point.getY().toString(16)));
+  }
+}
