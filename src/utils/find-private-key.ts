08166afed1c1342f6a6adae62f1b634cff73418b
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-09-23 11:28:45 +0400

    roll back

--- src/utils/find-private-key.ts
+++ src/utils/find-private-key.ts
@@ -4,33 +4,56 @@ import rList from '../../data/r-list.json';
 
 export const findPrivateKey = (
   Q: { x: bigint; y: bigint },
-) => {
-  let dBinary: string = '';
-  let indexes: number[] = [];
-  let candidateQ = Q;
+): string => {
+  let result: number[] = [];
+  let currentPoint = { x: Q.x, y: Q.y };
 
-  for (let i = rList.length - 1; i >= 0; i--) {
-    const candidateR = {
-      x: BigInt(rList[i].x),
-      y: BigInt(rList[i].y)
-    };
+  while (true) {
+    let found = false;
 
-    const previousPoint = Point.fromEllipticPoint(
-      findPreviousPoint(candidateQ, candidateR)
-    );
-
-    for (let k = i - 1; k >= 0; k--) {
-      const preCandidateR = {
-        x: BigInt(rList[k].x),
-        y: BigInt(rList[k].y)
+    for (let index = rList.length - 1; index >= 0; index--) {
+      const R = {
+        x: BigInt(rList[index].x),
+        y: BigInt(rList[index].y)
       };
 
-      if (
-        previousPoint.x === preCandidateR.x &&
-        previousPoint.y === preCandidateR.y
-      ) {
-        indexes.push(k);
-        dBinary += '1';
+      let previousPointElliptic = findPreviousPoint(currentPoint, R);
+      if (previousPointElliptic.x === null && previousPointElliptic.y === null) {
+        result.push(index);
+        let dBinary: string = '';
+        for (let k = 0; k < result.length; k++) {
+          if (k === 0) {
+            const zeros = '0'.repeat(rList.length - result[k] - 1).concat('1');
+            dBinary += zeros;
+          } else {
+            const zeros = '0'.repeat(result[k - 1] - result[k] -1).concat('1');
+            dBinary += zeros;
+          }
+
+          if (k === result.length - 1) {
+            const zeros = '0'.repeat(rList.length - dBinary.length);
+            dBinary += zeros;
+          }
+        }
+
+        return dBinary;
+      }
+
+      for (let i = index - 1; i >= 0; i--) {
+        const candidate = {
+          x: BigInt(rList[i].x),
+          y: BigInt(rList[i].y)
+        };
+
+        if (
+          previousPointElliptic.x === candidate.x &&
+          previousPointElliptic.y === candidate.y
+        ) {
+          result.push(index);
+          currentPoint = Point.fromEllipticPoint(previousPointElliptic);
+          found = true;
+          break;
+        }
       }
     }
   }
