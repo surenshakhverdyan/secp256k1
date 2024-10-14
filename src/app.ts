1bd3148754bc2946a8826e8b0a9bce177c3a87d2
Suren Hakhverdyan <154736461+surenshakhverdyan@users.noreply.github.com>
2024-10-14 12:30:04 +0400

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,6 @@ import { addressGenerate } from './utils/address.generate';
 import { privateKeyToWIF } from './utils/private-key-to-wif';
 import { findPrivateKey } from './utils/find-private-key';
 
-const q = {
-  x: 0x3E5AACFC20C77F3A6B24AED45D7CDA7AD889379681EB1C7968AABA1F5E28A78Fn,
-  y: 0x9B1ED65C930A32375FF8C16A2204A5F63FEA35404E5084A720AD1A1A4333E0E3n
-};
-
-findPrivateKey(q);
 const k = 0x000000000000000000000000000000000000000000000000000000000000000An;
 const Q = scalarMultiplication(k);
 
@@ -20,7 +14,7 @@ const privateKeyCompressed = privateKeyToWIF(k, true);
 const publicKeyCompressed = `${Q.y % 2n === 0n ? '02' : '03'}${Q.x.toString(16).padStart(64, '0').toUpperCase()}`;
 const addressCompressed = addressGenerate(publicKeyCompressed);
 
-console.log(`Private Key Hex: ${k.toString(16).toUpperCase()}`);
+console.log(`Private Key Hex: ${k.toString(16).padStart(64, '0').toUpperCase()}`);
 
 console.log('-'.repeat(155));
 console.log(`Private Key WIF Uncompressed: ${privateKeyUncompressed}`);
